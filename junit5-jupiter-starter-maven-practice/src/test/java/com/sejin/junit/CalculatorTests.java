3ac9ab37922e6fd6901a94c8a3f2b7a12f4a17f6
sejin <sejila@insightinfo.kr>
2019-12-03 14:35:14 +0900

    Add assertion with assertJ

--- junit5-jupiter-starter-maven-practice/src/test/java/com/sejin/junit/CalculatorTests.java
+++ junit5-jupiter-starter-maven-practice/src/test/java/com/sejin/junit/CalculatorTests.java
@@ -3,6 +3,7 @@ package com.sejin.junit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTests {
@@ -11,7 +12,12 @@ public class CalculatorTests {
     @DisplayName("1+1=2")
     void addsTwoNumber() {
         Calculator calculator = new Calculator();
+
+        //junit assertion
         assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
+
+        //assertJ assertion
+        assertThat(calculator.add(1, 1)).as("1 + 1 should equal 2").isEqualTo(2);
     }
 
 }
