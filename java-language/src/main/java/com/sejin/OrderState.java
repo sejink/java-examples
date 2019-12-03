03db239ccf03949a3c1a269d7060314af5f49d9e
sejin <sejila@insightinfo.kr>
2019-12-03 10:18:53 +0900

    Add enum tests

--- java-language/src/main/java/com/sejin/OrderState.java
+++ java-language/src/main/java/com/sejin/OrderState.java
@@ -1,5 +1,28 @@
 package com.sejin;
 
 public enum OrderState {
-    PAYMENT_WAITING
+    PAYMENT_WAITING (1) {
+        public boolean isShippingChangeable() {
+            return true;
+        }
+    },
+    SHIPPED (2);
+
+    private int state;
+
+    OrderState(int state) {
+        this.state = state;
+    }
+
+    public int getState() {
+        return this.state;
+    }
+
+    public boolean isShippingChangeable() {
+        return false;
+    }
+
+    public String toString() {
+       return String.valueOf(state);
+    }
 }
--- java-language/src/test/java/com/sejin/EnumTests.java
+++ java-language/src/test/java/com/sejin/EnumTests.java
@@ -2,6 +2,8 @@ package com.sejin;
 
 import org.junit.jupiter.api.Test;
 
+import static com.sejin.OrderState.PAYMENT_WAITING;
+import static com.sejin.OrderState.SHIPPED;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EnumTests {
@@ -10,5 +12,25 @@ public class EnumTests {
     void basicEnumTest() {
         OrderState orderState = OrderState.PAYMENT_WAITING;
         assertThat(OrderState.valueOf("PAYMENT_WAITING")).isEqualTo(orderState);
+    }
+
+    @Test
+    void compareEnumTypesWithOperator() {
+        OrderState orderState = OrderState.PAYMENT_WAITING;
+        assertThat(PAYMENT_WAITING == orderState).isTrue();
+        assertThat(PAYMENT_WAITING.equals(orderState)).isTrue();
+        assertThat(PAYMENT_WAITING == OrderState.SHIPPED).isFalse();
+    }
+
+    @Test
+    void customEnumMember() {
+        assertThat(PAYMENT_WAITING.getState()).isEqualTo(1);
+        assertThat(PAYMENT_WAITING.isShippingChangeable()).isTrue();
+        assertThat(SHIPPED.isShippingChangeable()).isFalse();
+    }
+
+    @Test
+    void overridingToString() {
+       assertThat(PAYMENT_WAITING.toString()).isEqualTo("1");
     }
 }
