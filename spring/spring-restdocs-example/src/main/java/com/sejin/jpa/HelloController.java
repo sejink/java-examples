14f4b2a227463b82f666b47c63d9a73ae80f9381
sejin <sejila@insightinfo.kr>
2019-12-12 18:25:01 +0900

    Add restdocs configuration

--- spring/spring-restdocs-example/src/main/java/com/sejin/jpa/HelloController.java
+++ spring/spring-restdocs-example/src/main/java/com/sejin/jpa/HelloController.java
@@ -1,13 +1,18 @@
 package com.sejin.jpa;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello, restdocs";
+    @GetMapping("/{name}")
+    public Map<String, String> hello(@PathVariable String name) {
+        return Collections.singletonMap("message", "Hello, restdocs");
     }
+
 }
